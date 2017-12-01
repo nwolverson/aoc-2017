64c6fd58c8360800112ad97c1a5705fdd89d44da
Nicholas Wolverson <nicholas.wolverson@gmail.com>
2017-12-01 20:37:01 +0000

    01 Turns out there's a part 2

--- 01/answer.js
+++ 01/answer.js
@@ -1,4 +1,9 @@
-module.exports = input => {
+exports.answer1 = input => {
     const arr = input.split("").map(Number);
     return arr.reduce((acc, x, i) => acc + (arr[(i+1) % arr.length] === x ? x : 0), 0);
+};
+
+exports.answer2 = input => {
+    const arr = input.split("").map(Number);
+    return arr.reduce((acc, x, i) => acc + (arr[(i + arr.length/2) % arr.length] === x ? x : 0), 0);
 };
