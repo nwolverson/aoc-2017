754d16d7f226939686722b159465847741757055
Nicholas Wolverson <nicholas.wolverson@gmail.com>
2017-12-03 11:47:54 +0000

    03 Racket part 2

--- 03/answer.rkt
+++ 03/answer.rkt
@@ -2,22 +2,45 @@
 (define (step x y n-full lim)
   (let ([n (min n-full lim)]
         [n1 (min (+ n-full 1) lim)])
-  (cond 
-    [(and (> x 0) (> y 0))
-     (list (- x n1) y n1)]
-    [(> x 0)
-     (list x (+ y n) n)
-     ]
-    [(and (<= x 0) (<= y 0))
-     (list (+ x n1) x n1)]
-    [else
-     (list x (- y n) n)
-     ])))
-(define (steps x y n c target)
+    (cond 
+      [(and (> x 0) (> y 0))
+       (list (- x n1) y n1)]
+      [(> x 0)
+       (list x (+ y n) n)
+       ]
+      [(and (<= x 0) (<= y 0))
+       (list (+ x n1) x n1)]
+      [else
+       (list x (- y n) n)
+       ])))
+(define (pos x y n c target)
   (if (< c target)
       (let ([res (step x y n (- target c))])
-            (steps (car res) (cadr res) (caddr res) (+ c (caddr res)) target))
+        (pos (car res) (cadr res) (caddr res) (+ c (caddr res)) target))
+      (cons x y)))
+(define (pos1 target) (pos 0 0 0 1 target))
 
-      (+ (abs x) (abs y))))
+(define (ans1 target)
+  (let ([res (pos 0 0 0 1 target)])
+    (+ (abs (car res)) (abs (cdr res)))))
 
-(define (ans1 target) (steps 0 0 0 1 target))
+(define (add-pairs p1 p2)
+  (cons (+ (car p1) (car p2))
+        (+ (cdr p1) (cdr p2))))
+(define (square-value grid pos)
+  (let ([adj '((1 . 0) (1 . 1) (0 . 1) (-1 . 1) (-1 . 0) (-1 . -1) (0 . -1) (1 . -1))])
+    (foldl + 0 (map (lambda (p)
+                      (hash-ref grid (add-pairs pos p) 0))
+                    adj))))
+
+(define (ans2-step grid n target)
+  (let ([val (square-value grid (pos1 n))])
+    (if (> val target)
+        val
+        (begin
+          (hash-set! grid (pos1 n) val)
+          (ans2-step grid (+ n 1) target)))))
+
+(define (ans2 target)
+  (let ([grid (make-hash '((( 0 . 0 ) . 1)))])
+    (ans2-step grid 2 target)))
