533d7f549b631dd46b03d5f95a0fe971590e59c1
Nicholas Wolverson <nicholas.wolverson@gmail.com>
2017-12-15 08:01:13 +0000

    14 part 1

--- 14/answer.jl
+++ 14/answer.jl
@@ -21,15 +21,31 @@ function hash(input)
     push!(input, 17, 31, 73, 47, 23)
     data = [UInt8(0):UInt8(255);]
     (i, skip) = (0, 0)
-    for j in 1:1
+    for j in 1:64
         (i, skip) = round(i, skip, data, input)
-        if j == 0
-            println(convert(Array{Int64}, data))
-        end
     end
-    println(reshape(data, (16,16)))
     numbers = reducedim(⊻, reshape(data, (16,16)), 1, 0)
-    bytes2hex(convert(Array{UInt8}, numbers)) # For some reason this becomes Int64 on reducedim
+    convert(Array{UInt8}, numbers) # For some reason this becomes Int64 on reducedim
 end
 
 # println(hash(b"31,2,85,1,80,109,35,63,98,255,0,13,105,254,128,33"))
+
+function answer1(input)
+    count = 0
+    for row in 0:127
+        count += mapreduce(count_ones, +, hash(convert(Array{UInt8},"$input-$row")))
+    end
+    count
+end
+
+function answer2(input)
+    bytes = map(row ->
+#        mapreduce(x -> digits(x, 2, 8), vcat, 
+            hash(convert(Array{UInt8},"$input-$row")), [1:127;])
+    bitarray = map(row -> mapreduce(by -> digits(by, 2, 8), vcat, row), bytes)
+    println(bitarray[1][1])
+    bitarray
+end
+
+println(answer1("oundnydw"))
+println(typeof(answer2("flqrgnkx")))
