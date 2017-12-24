93cd8cfa43b353c034953780b3c4f0f6b32e27da
Nicholas Wolverson <nicholas.wolverson@gmail.com>
2017-12-24 13:32:34 +0000

    22 Ruby - part 2

--- 22/answer.rb
+++ 22/answer.rb
@@ -1,44 +1,83 @@
 require 'numo/narray'
-lines = File.readlines("input.txt").map {|l| l.strip.split('').map { |x| x == '#' ? 1 : 0 } }
+lines = File.readlines("input.txt").map {|l| l.strip.split('').map { |x| x == '#' ? :infected : :clean } }
 arr = Numo::NArray[*lines]
-p arr
 
-x = arr.shape[0]/2 # 0..12..24
-y = arr.shape[1]/2
-dir = :up
+def decide(dir, node, left, right)
+    dir = if node == :infected then right[dir] else left[dir] end
+    updated = if node == :infected then :clean else :infected end
+    [dir, updated]
+end
+
+def decide2(dir, node, left, right)
+    opp = {:up => :down, :left => :right, :down => :up, :right => :left}
+
+    updated = if node == :clean then
+        :weakened
+    elsif node == :weakened then
+        :infected
+    elsif node == :infected then
+        :flagged
+    elsif node == :flagged then
+        :clean
+    else
+        raise ("bad node: " + node.to_s)
+    end
+    dir = if node == :clean then
+        left[dir]
+    elsif node == :weakened then
+        dir
+    elsif node == :infected then
+        right[dir]
+    elsif node == :flagged then
+        opp[dir]
+    else
+        raise ("bad node: " + node.to_s)
+    end
+    [dir, updated]
+end
 
 left = {:up => :left, :left => :down, :down => :right, :right => :up}
 right = left.invert
 
-move = {
-    :up => [0, -1],
-    :left => [-1, 0],
-    :down => [0, 1],
-    :right => [1, 0]
-}
+def answer(arr, n, &decider)
+    x = arr.shape[0]/2 # 0..12..24
+    y = arr.shape[1]/2
+    dir = :up
 
-infects = 0
-for i in 1..10000 do
-    dir = if arr[y, x] == 1 then right[dir] else left[dir] end
-    arr[y, x] = if arr[y, x] == 1 then 0 else 1 end
-    if arr[y, x] == 1 then
-        infects = infects + 1
-    end
-    x += move[dir][0]
-    y += move[dir][1]
+    move = {
+        :up => [0, -1],
+        :left => [-1, 0],
+        :down => [0, 1],
+        :right => [1, 0]
+    }
 
-    if y < 0 then
-        arr = arr.insert(0, 0, axis: 0)
-        y += 1
-    elsif x < 0 then
-        arr = arr.insert(0, 0, axis: 1)
-        x += 1
-    elsif y >= arr.shape[0]
-        arr = arr.insert(y, 0, axis: 0)
-    elsif x >= arr.shape[1]
-        arr = arr.insert(x, 0, axis: 1)
+    infects = 0
+    for i in 1..n do
+        dir, updated = yield(dir, arr[y,x])
+        arr[y,x] = updated
+        if updated == :infected then
+            infects = infects+1
+        end
+        x += move[dir][0]
+        y += move[dir][1]
+
+        if y < 0 then
+            arr = arr.insert(0, [:clean], axis: 0)
+            y += 1
+        elsif x < 0 then
+            arr = arr.insert(0, [:clean], axis: 1)
+            x += 1
+        elsif y >= arr.shape[0]
+            arr = arr.insert(y, [:clean], axis: 0)
+        elsif x >= arr.shape[1]
+            arr = arr.insert(x, [:clean], axis: 1)
+        end
     end
+    infects
 end
 
-p arr
-p infects
+# Part 1
+p answer(arr, 10000) { |dir, node| decide(dir, node, left, right) }
+
+# Part 2
+p answer(arr, 10000000) { |dir, node| decide2(dir, node, left, right) }
